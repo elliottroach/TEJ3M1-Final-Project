0c873e098efdddf093f6c042b570a1e38d7f8ced
elliottroach <elliott.roach@stu.ocsb.ca>
2026-06-04 18:12:13 +0000

    4's comp
    Co-authored-by: Caleb Andreas <Caleb-Andreas@users.noreply.github.com>
    Co-authored-by: Aimar Fernandez <Aimar-Fernandez@users.noreply.github.com>

--- 3 bit 2's comp/threeBitAdder2sComp.ino
+++ 3 bit 2's comp/threeBitAdder2sComp.ino
@@ -41,18 +41,18 @@ void setup() {
 
     // looping all possible inputs
     for (int a3Value = 0; a3Value <= 1; a3Value++) {
-        digitalWrite(pinA3, a3Value);
         for (int b3Value = 0; b3Value <= 1; b3Value++) {
-            digitalWrite(pinB3, b3Value);
             for (int a2Value = 0; a2Value <= 1; a2Value++) {
-                digitalWrite(pinA2, a2Value);
                 for (int b2Value = 0; b2Value <= 1; b2Value++) {
-                    digitalWrite(pinB2, b2Value);
                     for (int a1Value = 0; a1Value <= 1; a1Value++) {
-                        digitalWrite(pinA1, a1Value);
                         for (int b1Value = 0; b1Value <= 1; b1Value++) {
                             digitalWrite(pinB1, b1Value);
-
+                            digitalWrite(pinA3, a3Value);
+                            digitalWrite(pinB3, b3Value);
+                            digitalWrite(pinA2, a2Value);
+                            digitalWrite(pinB2, b2Value);
+                            digitalWrite(pinA1, a1Value);
+                          
                             // a value to decimal
                             if (a3Value == 1) {
                                 aDecimalValue = (2 * a2Value - 2) + (1 * a1Value - 1) - 1;
@@ -62,11 +62,11 @@ void setup() {
 
                             // b value to decimal
                             if (b3Value == 1) {
-                                bDecimalValue = ((-1 * b2Value + 1) * 2) + ((-1 * b1Value + 1) * 1) - 1;
+                                bDecimalValue = (2 * b2Value - 2) + ((1 * b1Value - 1) * 1) - 1;
                             } else {
                                 bDecimalValue = (b2Value * 2) + (b1Value * 1);
                             }
-
+                            
                             // show current calculation
                             Serial.print("\n" + String(a3Value) + String(a2Value) + String(a1Value) + " + " + String(b3Value) + String(b2Value) + String(b1Value) + "\n");
                             Serial.print("\n(" + String(aDecimalValue) + ")+(" + String(bDecimalValue) + ") = (" + String(aDecimalValue + bDecimalValue) + ")\n");
