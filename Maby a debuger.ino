e2bcc87ff444997dfb225677c0c9ca5af839572b
elliottroach <elliott.roach@stu.ocsb.ca>
2026-06-09 18:34:06 +0000

    somthing

---
+++ Maby a debuger.ino
@@ -0,0 +1,38 @@
+void setup() {
+  // put your setup code here, to run once:
+  const int pinA1 = 6;
+  const int pinA2 = 2;
+  const int pinA3 = 4;
+  const int pinA4 = 8;
+  // b pins
+  const int pinB1 = 7;
+  const int pinB2 = 3;
+  const int pinB3 = 5;
+  const int pinB4 = 9;
+
+  int a1Value = 0;
+  int a2Value = 0;
+  int a3Value = 0;
+  int a4Value = 0;
+  
+  int b1Value = 0;
+  int b2Value = 0;
+  int b3Value = 0;
+  int b4Value = 0;
+  
+  digitalWrite(pinA1, a1Value);
+  digitalWrite(pinA2, a2Value);
+  digitalWrite(pinA3, a3Value);
+  digitalWrite(pinA4, a4Value);
+  
+
+  digitalWrite(pinB1, b1Value);
+  digitalWrite(pinB2, b2Value);
+  digitalWrite(pinB3, b3Value);
+  digitalWrite(pinB4, b4Value);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
